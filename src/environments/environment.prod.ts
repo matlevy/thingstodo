45ecc8085f6bc3993bf904271963db5ac698193d
mlevy <servisol>
2017-05-30 13:34:51 +0100

    firebase deploy setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+    apiKey: "AIzaSyDTEX6K2dDjesLH1NWAybIruQR3XizA1U4",
+    authDomain: "thingstodo-ddb6b.firebaseapp.com",
+    databaseURL: "https://thingstodo-ddb6b.firebaseio.com",
+    projectId: "thingstodo-ddb6b",
+    storageBucket: "thingstodo-ddb6b.appspot.com",
+    messagingSenderId: "585599997005"
+  }
 };
