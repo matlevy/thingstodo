cfbe896dfe5768996ad6fb6a2e18c0cae0680c0b
mlevy <servisol>
2017-06-05 17:22:49 +0100

    things service base test

--- src/app/things-services/things.service.spec.ts
+++ src/app/things-services/things.service.spec.ts
@@ -1,15 +1,25 @@
 import { TestBed, inject } from '@angular/core/testing';
-
 import { ThingsService } from './things.service';
+import { UserService } from './user.service';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { ThingsServicesModule } from './things-services.module';
+import { AngularFireModule } from 'angularfire2';
+import { AppModule } from "../app.module";
 
-xdescribe('ThingsService', () => {
+describe('ThingsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ThingsService]
+      providers: [ThingsService,UserService],
+      imports: [
+        AngularFireDatabaseModule,
+        ThingsServicesModule,
+        AppModule
+      ],
     });
   });
 
   it('should be created', inject([ThingsService], (service: ThingsService) => {
     expect(service).toBeTruthy();
+    expect(service.user).toBeTruthy();
   }));
 });
